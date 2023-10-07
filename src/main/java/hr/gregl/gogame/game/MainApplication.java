7f36d3725e55ab4c0be86e09dc3a54c0b3e8e5cb
Albert Gregl <albert.gregl@gmail.com>
2023-10-07 22:19:19 +0200

    Welcome screen

--- src/main/java/hr/gregl/gogame/game/MainApplication.java
+++ src/main/java/hr/gregl/gogame/game/MainApplication.java
@@ -3,17 +3,23 @@ package hr.gregl.gogame.game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Image appIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/icon.png")));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
         stage.setTitle("Go - 圍棋");
         stage.setScene(scene);
+        stage.getIcons().add(appIcon);
+
         stage.show();
     }
 
